4252aeef32a4e128e72746e1d7920dc6118fcfe3
Stefano and jmourell <steppashr+j.mourell@gmail.com>
2019-07-09 19:30:49 +0900

    Vending Machine, new test on change, all tests pass

--- VendingMachine.js
+++ VendingMachine.js
@@ -6,32 +6,30 @@
 */
 const juice = { name: `Apple Juice`, price: 350, count: 5 };
 const coffee = { name: "Tully's", price: 250, count: 7 };
+const inventory = [[juice, coffee]];
 
 class VendingMachine {
   constructor(balance = 0) {
     this._balance = balance;
     this.till = {
-      10: 0,
-      50: 0,
-      100: 0,
-      500: 0,
+      10: 50,
+      50: 50,
+      100: 50,
+      500: 50,
     };
     this._selectedRow = "";
     // 0 is the default value
     this._selectedCol = 0;
-    this.inventory = [[juice, coffee]];
   }
   /* Assumes a single coin is inserted*/
   insertCoin(coin) {
     this.till[coin]++;
-    this.calculateBalance();
+    this.balance += coin;
     // console.log(this._balance);
   }
-  calculateBalance() {
-    this._balance = 0;
-    for (let key in this.till) {
-      this._balance += this.till[key] * key;
-    }
+
+  set balance(coin) {
+    this._balance = coin;
   }
   get balance() {
     return this._balance;
@@ -68,8 +66,27 @@ class VendingMachine {
         break;
     }
     let col = this.selectedColumn - 1;
-    return this.inventory[row][col].name;
+    inventory[row][col].count--;
+    this.balance -= inventory[row][col].price;
+    return inventory[row][col].name;
+  }
+  change() {
+    let result = {};
+    const coins = [];
+    for (let key in this.till) {
+      coins.push(Number(key));
+    }
+    coins.sort((a, b) => b - a);
+
+    for (let coin of coins) {
+      result[coin] = 0;
+      while (this.balance >= coin && this.balance != 0) {
+        this.balance -= coin;
+        ++result[coin];
+      }
+    }
+    return result;
   }
 }
 
-module.exports = VendingMachine;
+module.exports = { VendingMachine, inventory };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const VendingMachine = require("../VendingMachine");
+const { VendingMachine, inventory } = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
@@ -11,10 +11,10 @@ describe("vending machine", () => {
 
     // Assert
     expect(machine.till).to.deep.equal({
-      10: 0,
-      50: 0,
-      100: 0,
-      500: 1,
+      10: 50,
+      50: 50,
+      100: 50,
+      500: 51,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
     // expect(console.log.calledWith("500")).to.be.true; // Use an ES6 getter
@@ -46,6 +46,25 @@ describe("vending machine", () => {
     machine.insertCoin(500);
     machine.pressButton("A");
     machine.pressButton(1);
-    expect(machine.inventory[0][0].count).to.be.equal(4);
+    machine.output();
+    expect(inventory[0][0].count).to.be.equal(3);
+  });
+  it("should return the change", () => {
+    const machine = new VendingMachine();
+    machine.insertCoin(100);
+    machine.insertCoin(50);
+    machine.insertCoin(50);
+    machine.insertCoin(100);
+    machine.insertCoin(100);
+    machine.insertCoin(100);
+    machine.pressButton("A");
+    machine.pressButton(1);
+    machine.output();
+    expect(machine.change()).to.deep.equal({
+      10: 0,
+      50: 1,
+      100: 1,
+      500: 0,
+    });
   });
 });
